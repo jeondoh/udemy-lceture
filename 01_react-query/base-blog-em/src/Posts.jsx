b73030c82f2b8618c1514f13cb81649db45ed0ef
jeondoh <jundo1302@gmail.com>
2022-09-01 00:24:34 +0900

    compare with staleTime, cacheTime

--- 01_react-query/base-blog-em/src/Posts.jsx
+++ 01_react-query/base-blog-em/src/Posts.jsx
@@ -22,9 +22,16 @@ export function Posts() {
                     isLoading 은 isFetching 의 부분집합
         isError : 오류가 있을시에 기본적으로 3번 재요청 후 에러 반환
   */
+  // options
+  /*
+        staleTime(default 0) : 데이터가 만료됐다고 판단하기 전까지 허용하는 시간 > 시간 초과시 데이터 만료(stale) 상태
+                                즉 데이터가 fresh > stale 상태로 변경되는데 걸리는 시간
+        cacheTime(default 5분) : 데이터가 inactive 상태일 때 캐싱된 상태로 남아있는 시간 > 시간 초과시 가비지 콜렉터로 수집
+  */
   const { data, isLoading, isFetching, error, isError } = useQuery(
     "posts",
-    fetchPosts
+    fetchPosts,
+    { staleTime: 2000, cacheTime: 3000 }
   );
 
   return (
