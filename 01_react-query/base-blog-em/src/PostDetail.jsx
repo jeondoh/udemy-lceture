e98788d787839b8ccd890ff14454526037698c99
jeondoh <jundo1302@gmail.com>
2022-09-01 15:45:55 +0900

    feat: set querykey, pagination

--- 01_react-query/base-blog-em/src/PostDetail.jsx
+++ 01_react-query/base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -22,20 +24,27 @@ async function updatePost(postId) {
 }
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, isLoading } = useQuery(["comments", post.id], () =>
+    fetchComments(post.id)
+  );
 
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
-      <p>{post.body}</p>
-      <h4>Comments</h4>
-      {data.map((comment) => (
-        <li key={comment.id}>
-          {comment.email}: {comment.body}
-        </li>
-      ))}
+      {isLoading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <>
+          <h3 style={{ color: "blue" }}>{post.title}</h3>
+          <button>Delete</button> <button>Update title</button>
+          <p>{post.body}</p>
+          <h4>Comments</h4>
+          {data.map((comment) => (
+            <li key={comment.id}>
+              {comment.email}: {comment.body}
+            </li>
+          ))}
+        </>
+      )}
     </>
   );
 }
