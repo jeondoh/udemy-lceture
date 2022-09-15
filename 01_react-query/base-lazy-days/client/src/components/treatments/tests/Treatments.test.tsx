83571e8f94f9d64b7c51b962cfbb38fad93cad23
jeondoh <jundo1302@gmail.com>
2022-09-15 23:29:26 +0900

    feat: test for render treatments query

--- 01_react-query/base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ 01_react-query/base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -3,6 +3,10 @@ import { render, screen } from '@testing-library/react';
 import { renderWithQueryClient } from '../../../test-utils';
 import { Treatments } from '../Treatments';
 
-test('renders response from query', () => {
+test('renders response from query', async () => {
   renderWithQueryClient(<Treatments />);
+  const treatmentTitles = await screen.findByRole('heading', {
+    name: /massage|facial|scrub/i,
+  });
+  expect(treatmentTitles).toHaveLength(3);
 });
